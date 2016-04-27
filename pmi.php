2c7c0b554c6312661f730b653a0f1d33b209a788
VxeR <vxer@seznam.cz>
2016-04-27 20:18:06 +0200

    bodova vzajemna informace cz

---
+++ pmi.php
@@ -0,0 +1,49 @@
+<?php
+ini_set('memory_limit','1024M');
+
+$uni = array();
+$bi = array();
+$words = array();
+$n = 0;
+
+$file = fopen("czech_unigramy.txt", "r") or exit("Unable to open file!");
+while(!feof($file)) {
+	$f = fgets($file);
+	if($f != '') {
+		$words = explode(' ', $f);
+		$uni[$words[0]] = $words[1];
+		$n += $words[1];
+	}
+  }
+fclose($file);
+
+$file = fopen("czech_bigramy.txt", "r") or exit("Unable to open file!");
+while(!feof($file)) {
+	$f = fgets($file);
+	if($f != '') {
+		$words = explode(' ', $f);
+		if($words[1] != '') {
+			$val = $words[0].','.$words[1];
+			$bi[$val] = $words[2];
+		}
+		else
+			echo "Chybny bigram: "$words[0].','.$words[1].' '.$words[2]."<br>";
+	}
+}
+fclose($file);
+$pmi = array();
+
+foreach($bi as $key => $value) {
+	$w = explode(',',$key);
+	$pmi[$key] = log($n * ($value / ($uni[$w[0]] * $uni[$w[1]])));
+}
+arsort($pmi);
+
+$max = reset($pmi);
+foreach ($pmi as $key => $value) {
+	if($value >= $max)
+		echo "PMI(".$key.") = ".$value."<br>";
+}
+ 
+
+?>
